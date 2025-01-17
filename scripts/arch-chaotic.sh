67b1e378ea3379e37d5225ed9d5fa84b605458bc
askpng <81590263+askpng@users.noreply.github.com>
2025-01-18 02:49:31 +0700

    arch-chaotic.sh: Update

--- scripts/arch-chaotic.sh
+++ scripts/arch-chaotic.sh
@@ -2,17 +2,35 @@
 
 set -oue pipefail
 
-sudo pacman -Syu git base-devel --noconfirm
-
-sed -i 's/#Color/Color/g' /etc/pacman.conf && \
-    printf "[multilib]\nInclude = /etc/pacman.d/mirrorlist\n" | tee -a /etc/pacman.conf && \
-    sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/g' /etc/makepkg.conf
-
+# Init pacman keys
 sudo pacman-key --init
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
-# sudo pacman-key --populate chaotic-aur
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-
-echo -e "[chaotic-aur]\nSigLevel = Required DatabaseOptional\nServer = https://mirror.rackspace.com/chaotic-aur/\$arch" >> /etc/pacman.conf
+# Chaotic-AUR
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+# Append multilib to pacman.conf
+sudo sed -i '87i [multilib]\nInclude = /etc/pacman.d/mirrorlist' /etc/pacman.conf
+# Append Chaotic-AUR pacman.conf
+sudo sed -i '96i [chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' /etc/pacman.conf
+# pacman.conf color
+sudo sed -i 's/#Color/Color/g' /etc/pacman.conf
+# makepkg.conf makeflags nproc
+sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/g' /etc/makepkg.conf
+sudo pacman -Syy
+# Install paru 
+sudo pacman -S paru --noconfirm
+# Install steamcmd & Vesktop
+paru -S steamcmd vesktop --noconfirm
+## Steam
+# sed -i 's@ (Runtime)@@g' /usr/share/applications/steam.desktop
+## Vesktop
+# VESKTOP_DESKTOP_FILE="/usr/share/applications/vesktop.desktop"
+# if [[ -f "$VESKTOP_DESKTOP_FILE" ]]; then
+#    sed -i '/^Exec=/s/$/ --ozone-platform-hint=auto --enable-features=VaapiVideoDecodeLinuxGL,VaapiVideoEncoder,WebRTCPipeWireCapturer --enable-gpu-rasterization --ignore-gpu-blocklist --enable-zero-copy/' "$VESKTOP_DESKTOP_FILE"
+# fi
+# Directories and exports
+# distrobox-export --app steam
+# mkdir -p ~/.steam
+# distrobox-export --bin /usr/bin/steamcmd --export-path ~/.steam
+# mv ~/.steam/steamcmd ~/.steam/steamcmd.sh
