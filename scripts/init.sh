958c54759553e7952266ba1c0d553e46fe32c70e
askpng <81590263+askpng@users.noreply.github.com>
2025-01-18 02:49:42 +0700

    init.sh: Update

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,31 +1,49 @@
 #!/bin/bash
 
-
 set -oue pipefail
 
-# Update pacman configuration and install essential packages
-sed -i 's/#Color/Color/g' /etc/pacman.conf && \
-    printf "[multilib]\nInclude = /etc/pacman.d/mirrorlist\n" | tee -a /etc/pacman.conf && \
-    sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/g' /etc/makepkg.conf
-
 # Init pacman
-pacman-key --init && \
-pacman-key --populate archlinux
+pacman-key --init
 
-# Install Chaotic-AUR keyring and mirrorlist
-curl -fsSL https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst -o /tmp/chaotic-keyring.pkg.tar.zst && \
-    pacman -U --noconfirm /tmp/chaotic-keyring.pkg.tar.zst && \
-    pacman-key --populate chaotic-aur && \
-    curl -fsSL https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst -o /tmp/chaotic-mirrorlist.pkg.tar.zst && \
-    pacman -U --noconfirm /tmp/chaotic-mirrorlist.pkg.tar.zst && \
-    rm -f /tmp/chaotic-keyring.pkg.tar.zst /tmp/chaotic-mirrorlist.pkg.tar.zst
+# Chaotic-AUR
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 
+# Append multilib to pacman.conf
+sed -i '87i [multilib]\nInclude = /etc/pacman.d/mirrorlist' /etc/pacman.conf
 
-# Add Chaotic-AUR repository to pacman.conf
-echo -e "[chaotic-aur]\nSigLevel = Required DatabaseOptional\nServer = https://mirror.rackspace.com/chaotic-aur/\$arch" >> /etc/pacman.conf
+# Append Chaotic-AUR pacman.conf
+sed -i '96i [chaotic-aur]\nSigLevel = Never\nInclude = /etc/pacman.d/chaotic-mirrorlist' /etc/pacman.conf
 
-# Update pacman keyring
-pacman-key --init && pacman-key --populate archlinux chaotic-aur
+# Color in pacman.conf
+sed -i 's/#Color/Color/g' /etc/pacman.conf
+
+# MAKEFLAGS processor instructions
+sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/g' /etc/makepkg.conf
+
+# Update pacman database
+pacman -Syy
+
+# Install paru
+pacman -S paru --noconfirm
+
+# Install steamcmd and Vesktop
+paru -S steamcmd vesktop --noconfirm
+
+# Exports
+
+# Steam
+# sed -i 's@ (Runtime)@@g' /usr/share/applications/steam.desktop
+## Vesktop
+# VESKTOP_DESKTOP_FILE="/usr/share/applications/vesktop.desktop"
+# if [[ -f "$VESKTOP_DESKTOP_FILE" ]]; then
+#    sed -i '/^Exec=/s/$/ --ozone-platform-hint=auto --enable-features=VaapiVideoDecodeLinuxGL,VaapiVideoEncoder,WebRTCPipeWireCapturer --enable-gpu-rasterization --ignore-gpu-blocklist --enable-zero-copy/' "$VESKTOP_DESKTOP_FILE"
+# fi
+# Directories and exports
+# distrobox-export --app steam
+# mkdir -p ~/.steam
+# distrobox-export --bin /usr/bin/steamcmd --export-path ~/.steam
+# mv ~/.steam/steamcmd ~/.steam/steamcmd.sh
 
 # Add user and configure sudo permissions
 useradd -m --shell=/bin/bash build && usermod -L build && \
