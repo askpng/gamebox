f4525b4e91be9f5b5a1633df7689cbf7776ce826
askpng <81590263+askpng@users.noreply.github.com>
2025-01-18 01:22:45 +0700

    init.sh: Update

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,6 +8,10 @@ sed -i 's/#Color/Color/g' /etc/pacman.conf && \
     printf "[multilib]\nInclude = /etc/pacman.d/mirrorlist\n" | tee -a /etc/pacman.conf && \
     sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/g' /etc/makepkg.conf
 
+# Init pacman
+pacman-key --init && \
+pacman-key --populate archlinux
+
 # Install Chaotic-AUR keyring and mirrorlist
 curl -fsSL https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst -o /tmp/chaotic-keyring.pkg.tar.zst && \
     pacman -U --noconfirm /tmp/chaotic-keyring.pkg.tar.zst && \
@@ -22,9 +26,6 @@ echo -e "[chaotic-aur]\nSigLevel = Required DatabaseOptional\nServer = https://m
 
 # Update pacman keyring
 pacman-key --init && pacman-key --populate archlinux chaotic-aur
-
-# Add Chaotic-AUR repository to pacman.conf with signature checking enabled
-echo -e "[chaotic-aur]\nSigLevel = Required DatabaseOptional\nServer = https://mirror.rackspace.com/chaotic-aur/\$arch" >> /etc/pacman.conf
 
 # Add user and configure sudo permissions
 useradd -m --shell=/bin/bash build && usermod -L build && \
