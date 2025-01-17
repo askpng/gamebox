9c36eb19ea1cd990a14259efcdf9b72bd9e09af9
askpng <81590263+askpng@users.noreply.github.com>
2025-01-18 02:54:21 +0700

    arch-chaotic.sh: Update

--- scripts/arch-chaotic.sh
+++ scripts/arch-chaotic.sh
@@ -23,14 +23,14 @@ sudo pacman -S paru --noconfirm
 # Install steamcmd & Vesktop
 paru -S steamcmd vesktop --noconfirm
 ## Steam
-# sed -i 's@ (Runtime)@@g' /usr/share/applications/steam.desktop
+sed -i 's@ (Runtime)@@g' /usr/share/applications/steam.desktop
 ## Vesktop
-# VESKTOP_DESKTOP_FILE="/usr/share/applications/vesktop.desktop"
-# if [[ -f "$VESKTOP_DESKTOP_FILE" ]]; then
-#    sed -i '/^Exec=/s/$/ --ozone-platform-hint=auto --enable-features=VaapiVideoDecodeLinuxGL,VaapiVideoEncoder,WebRTCPipeWireCapturer --enable-gpu-rasterization --ignore-gpu-blocklist --enable-zero-copy/' "$VESKTOP_DESKTOP_FILE"
-# fi
+VESKTOP_DESKTOP_FILE="/usr/share/applications/vesktop.desktop"
+if [[ -f "$VESKTOP_DESKTOP_FILE" ]]; then
+   sed -i '/^Exec=/s/$/ --ozone-platform-hint=auto --enable-features=VaapiVideoDecodeLinuxGL,VaapiVideoEncoder,WebRTCPipeWireCapturer --enable-gpu-rasterization --ignore-gpu-blocklist --enable-zero-copy/' "$VESKTOP_DESKTOP_FILE"
+fi
 # Directories and exports
-# distrobox-export --app steam
-# mkdir -p ~/.steam
-# distrobox-export --bin /usr/bin/steamcmd --export-path ~/.steam
-# mv ~/.steam/steamcmd ~/.steam/steamcmd.sh
+distrobox-export --app steam
+mkdir -p ~/.steam
+distrobox-export --bin /usr/bin/steamcmd --export-path ~/.steam
+mv ~/.steam/steamcmd ~/.steam/steamcmd.sh
