ac93e1a66208166abf3a2ae51d356c930ea5f002
askpng <81590263+askpng@users.noreply.github.com>
2025-01-18 01:11:22 +0700

    Create arch-chaotic.sh

---
+++ scripts/arch-chaotic.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -oue pipefail
+
+sudo pacman -Syu git base-devel
+
+sed -i 's/#Color/Color/g' /etc/pacman.conf && \
+    printf "[multilib]\nInclude = /etc/pacman.d/mirrorlist\n" | tee -a /etc/pacman.conf && \
+    sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/g' /etc/makepkg.conf
+
+sudo pacman-key --init
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+# sudo pacman-key --populate chaotic-aur
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+echo -e "[chaotic-aur]\nSigLevel = Required DatabaseOptional\nServer = https://mirror.rackspace.com/chaotic-aur/\$arch" >> /etc/pacman.conf
