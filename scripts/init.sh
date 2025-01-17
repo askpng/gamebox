c041b0eaf673ffd5c4110c0d76f52d0821468d40
askpng <81590263+askpng@users.noreply.github.com>
2025-01-18 01:00:10 +0700

    init.sh: Update

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,15 +8,14 @@ sed -i 's/#Color/Color/g' /etc/pacman.conf && \
     printf "[multilib]\nInclude = /etc/pacman.d/mirrorlist\n" | tee -a /etc/pacman.conf && \
     sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$(nproc)"/g' /etc/makepkg.conf
 
-# Install Chaotic-AUR keyring
+# Install Chaotic-AUR keyring and mirrorlist
 curl -fsSL https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst -o /tmp/chaotic-keyring.pkg.tar.zst && \
     pacman -U --noconfirm /tmp/chaotic-keyring.pkg.tar.zst && \
-    rm -f /tmp/chaotic-keyring.pkg.tar.zst
-
-# Install Chaotic-AUR mirrorlist
-curl -fsSL https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst -o /tmp/chaotic-mirrorlist.pkg.tar.zst && \
+    pacman-key --populate chaotic-aur && \
+    curl -fsSL https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst -o /tmp/chaotic-mirrorlist.pkg.tar.zst && \
     pacman -U --noconfirm /tmp/chaotic-mirrorlist.pkg.tar.zst && \
-    rm -f /tmp/chaotic-mirrorlist.pkg.tar.zst
+    rm -f /tmp/chaotic-keyring.pkg.tar.zst /tmp/chaotic-mirrorlist.pkg.tar.zst
+
 
 # Add Chaotic-AUR repository to pacman.conf
 echo -e "[chaotic-aur]\nSigLevel = Required DatabaseOptional\nServer = https://mirror.rackspace.com/chaotic-aur/\$arch" >> /etc/pacman.conf
