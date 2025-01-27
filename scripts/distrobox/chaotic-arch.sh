721bb9978b01572a95c6c3e9f105228c75644a64
askpng <81590263+askpng@users.noreply.github.com>
2025-01-27 20:50:51 +0700

    Update script

--- scripts/distrobox/chaotic-arch.sh
+++ scripts/distrobox/chaotic-arch.sh
@@ -8,3 +8,15 @@ sudo pacman -U 'https://geo-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.ta
 sudo pacman -U 'https://geo-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 sudo sed -i '96i [chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' /etc/pacman.conf
 sudo pacman -Syy --noconfirm
+sudo pacman -S paru --noconfirm
+sudo paru -S --needed \
+    adwsteamgtk \
+    blackbox-terminal \
+    downgrade \
+    morewaita-icon-theme \
+    steamcmd \
+    steamtinkerlaunch \
+    aur/vesktop-electron \
+#    aur/pingu \
+#    aur/hatt-bin \
+    --noconfirm
