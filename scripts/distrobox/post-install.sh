838a42db99bbda5c9cf2bd7287a7e366eaf210bc
askpng <81590263+askpng@users.noreply.github.com>
2025-01-30 19:24:44 +0700

    Update post-install.sh

--- scripts/distrobox/post-install.sh
+++ scripts/distrobox/post-install.sh
@@ -2,7 +2,7 @@
 
 set -oue pipefail
 
-# Distrobox setup
+# Links
 
 ln -sf /usr/bin/distrobox-host-exec /usr/bin/flatpak && \
     ln -sf /usr/bin/xdg-open /usr/bin/distrobox-xdg-open && \
@@ -10,11 +10,50 @@ ln -sf /usr/bin/distrobox-host-exec /usr/bin/flatpak && \
     ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/conmon && \
     ln -sf /usr/bin/distrobox-host-exec /usr/local/bin/podman
 
-# Directories and exports
-distrobox-export --app steam
-mkdir -p ~/.steam
-distrobox-export --bin /usr/bin/steamcmd --export-path ~/.steam
-mv ~/.steam/steamcmd ~/.steam/steamcmd.sh
-distrobox-export --app vesktop
+# Steam preps
+
+if ! rm -drf ~/.steam .steampath .steampid; then
+  echo "Error: Failed to remove Steam files/directories"
+  exit 1
+fi
+
+if ! mkdir -p ~/.steam/; then
+  echo "Error: Failed to create ~/.steam/ directory"
+  exit 1
+fi
+
+if ! distrobox-export --bin /usr/bin/steamcmd --export-path ~/.steam/; then
+  echo "Error: Failed to export steamcmd"
+  exit 1
+fi
+
+if ! mv ~/.steam/steamcmd ~/.steam/steamcmd.sh; then
+  echo "Error: Failed to rename steamcmd"
+  exit 1
+fi
+
+echo "Successfully purged and recreated Steam files/directories"
+
+# Exports
+
+distrobox-export --app blackbox
+
 distrobox-export --app celluloid
 
+distrobox-export --app hatt
+
+distrobox-export --app lutris
+
+distrobox-export --app megabasterd
+
+distrobox-export --app "steam"
+
+distrobox-export --app vesktop
+
+distrobox-export --bin /bin/glow --export-path $HOME/.local.bin
+
+distrobox-export --bin /bin/tldr --export-path $HOME/.local.bin
+
+distrobox-export --bin /bin/pingu --export-path $HOME/.local.bin
+
+distrobox-export --bin /bin/eza --export-path $HOME/.local.bin
