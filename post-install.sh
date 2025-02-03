2afb4e89bdb2b678193e7acd9efaa88123ec35e8
askpng <81590263+askpng@users.noreply.github.com>
2025-02-04 04:10:01 +0700

    Scripts

--- post-install.sh
+++ post-install.sh
@@ -2,6 +2,18 @@
 
 set -oue pipefail
 
+# Chaotic AUR
+
+sudo pacman-key --init
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://geo-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
+sudo pacman -U 'https://geo-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+sudo sed -i '96i [chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' /etc/pacman.conf
+sudo sed -i '4s/^/# /' /etc/pacman.d/chaotic-mirrorlist
+sed '2s/Name=Discord (linux-discord-rich-presence)/Name=Discord (RPC)/' /usr/share/applications/linux-discord-rich-presence.desktop
+sudo pacman -Syyu --noconfirm
+
 # Steam preps
 if ! rm -drf ~/.config/steamtinkerlaunch; then
   echo "Error: Failed to remove steamtinkerlaunch files/directories"
@@ -46,11 +58,10 @@ distrobox-export --bin /usr/bin/blackbox
 distrobox-export --bin /usr/bin/glow
 distrobox-export --bin /usr/bin/tldr
 distrobox-export --bin /usr/bin/pingu
-
 # Gamebox exports
+distrobox-export --app "/usr/share/applications/linux-discord-rich-presence.desktop" -el none
 distrobox-export --app steam -el none
 distrobox-export --app SGDBoop -el none
 distrobox-export --app lutris -el none
 distrobox-export --app protonplus -el none
-
 echo "Exports successful!"
--- scripts/chaotic.sh
+++ scripts/chaotic.sh
@@ -2,21 +2,11 @@
 
 set -oue pipefail
 
+sudo pacman-key --init
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://geo-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
 sudo pacman -U 'https://geo-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 sudo sed -i '96i [chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' /etc/pacman.conf
-sudo pacman -Syy --noconfirm
-sudo pacman -S paru --noconfirm
-sudo paru -S --needed \
-    adwsteamgtk \
-    blackbox-terminal \
-    downgrade \
-    morewaita-icon-theme \
-    steamcmd \
-    steamtinkerlaunch \
-    aur/vesktop-electron \
-#    aur/pingu \
-#    aur/hatt-bin \
-    --noconfirm
+sudo sed -i '4s/^/# /' /etc/pacman.d/chaotic-mirrorlist
+sudo pacman -Syyu --noconfirm
