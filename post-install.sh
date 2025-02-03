81be6f36e0a6eeb4ba7b5ab1d80e2eed158b2293
askpng <81590263+askpng@users.noreply.github.com>
2025-02-03 20:37:48 +0700

    Update post-install.sh

---
+++ post-install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -oue pipefail
+
+# Steam preps
+if ! rm -drf ~/.config/steamtinkerlaunch; then
+  echo "Error: Failed to remove steamtinkerlaunch files/directories"
+  exit 1
+fi
+
+if ! rm -drf ~/.wine; then
+  echo "Error: Failed to remove .wine files/directories"
+  exit 1
+fi
+
+if ! rm -drf ~/.steam .steampath .steampid; then
+  echo "Error: Failed to remove Steam files/directories"
+  exit 1
+fi
+
+if ! mkdir -p ~/.steam/; then
+  echo "Error: Failed to create ~/.steam/ directory"
+  exit 1
+fi
+
+if ! distrobox-export --bin /usr/bin/steamcmd --export-path ~/.steam/; then
+  echo "Error: Failed to export steamcmd"
+  exit 1
+fi
+
+if ! mv ~/.steam/steamcmd ~/.steam/steamcmd.sh; then
+  echo "Error: Failed to rename steamcmd"
+  exit 1
+fi
+
+echo "Successfully purged and recreated Steam files/directories!"
+
+# Box exports
+distrobox-export --app celluloid -el none
+distrobox-export --app hatt -el none
+distrobox-export --app "/usr/share/applications/jdownloader.desktop" -el none
+distrobox-export --app megabasterd -el none
+distrobox-export --app vesktop -el none
+distrobox-export --bin /usr/bin/btop
+distrobox-export --bin /usr/bin/blackbox
+distrobox-export --bin /usr/bin/glow
+distrobox-export --bin /usr/bin/tldr
+distrobox-export --bin /usr/bin/pingu
+
+# Gamebox exports
+distrobox-export --app "/usr/share/applications.desktop/steam.desktop" -el none
+distrobox-export --app "/usr/share/applications.desktop/SGDBoop.desktop" -el none
+distrobox-export --app lutris -el none
+distrobox-export --app "/usr/share/applications.desktop/protonplus.desktop" -el none
+distrobox-export --app "/usr/share/applications.desktop/ryujinx.desktop" -el none
+
+echo "Exports successful!"
